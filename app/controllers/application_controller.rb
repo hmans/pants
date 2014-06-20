abc1e293a8c949fc4a7692b36cd42ee0c007b399
Hendrik Mans <hendrik@mans.de>
2014-06-20 22:37:25 +0200

    View posts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  respond_to :html
+
+  def current_user
+    nil
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,8 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    @posts = @posts.order('created_at DESC')
+    respond_with @posts
+  end
 end
