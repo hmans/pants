6e68ff5a2a26b60d057689e2749fb31cf645a163
Hendrik Mans <hendrik@mans.de>
2014-06-20 22:10:35 +0200

    Disable some generators we don't need.

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,11 @@ module Pants
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Disable some generators we don't need.
+    #
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
+    config.generators.helper      = false
   end
 end
