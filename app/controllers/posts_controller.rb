0dead7febcddc36d339771ab1978b3049716cd68
Hendrik Mans <hendrik@mans.de>
2014-06-20 22:57:10 +0200

    Edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,21 @@ class PostsController < ApplicationController
     @posts = @posts.order('created_at DESC')
     respond_with @posts
   end
+
+  def show
+    respond_with @post
+  end
+
+  def edit
+    respond_with @post
+  end
+
+  def update
+    @post.update_attributes(post_params)
+    respond_with @post
+  end
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
