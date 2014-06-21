ae501d19ada16fb8b65a5487291299290a646860
Hendrik Mans <hendrik@mans.de>
2014-06-21 23:48:45 +0200

    Add a first spec. We need to start somewhere, right?

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#sha' do
+    it 'is automatically generated when validating a post instance' do
+      post = build(:post)
+      expect(post.sha).to be_blank
+      post.valid?
+      expect(post.sha).to_not be_blank
+    end
+  end
 end
