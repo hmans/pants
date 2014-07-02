3c684186c7856db66ef8105afe1efa791f63a639
Hendrik Mans <hendrik@mans.de>
2014-07-02 16:16:31 +0200

    Clean up ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,26 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # By default, only respond to HTML requests.
   respond_to :html
 
-  helper_method :current_site, :current_user
+  concerning :ErrorHandling do
+    included do
+      rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+      rescue_from CanCan::AccessDenied do |exception|
+        @error = exception.message
+        @page_title = "Error"
+        render 'error', status: 403
+      end
+    end
+
+    def render_404
+      @error = "Not found. I'm sorry."
+      @page_title = "Not Found"
+      render 'error', status: 404
+    end
+  end
 
   before_filter do
     if current_site.blank?
@@ -21,49 +36,49 @@ class ApplicationController < ActionController::Base
     I18n.locale = current_site.locale || 'en'
   end
 
-  rescue_from CanCan::AccessDenied do |exception|
-    @error = exception.message
-    @page_title = "Error"
-    render 'error', status: 403
-  end
+  concerning :CurrentUser do
+    included do
+      helper_method :current_user
+    end
 
-  def render_404
-    @error = "Not found. I'm sorry."
-    @page_title = "Not Found"
-    render 'error', status: 404
-  end
-
-  def current_user
-    @current_user ||= begin
-      if session[:current_user].present?
-        User.find_by!(domain: session[:current_user])
-      elsif cookies[:login_user].present? && cookies[:login_domain] == current_site.domain
-        user = User.find_by!(domain: cookies[:login_user])
-        session[:current_user] = user.domain
-        user
+    def current_user
+      @current_user ||= begin
+        if session[:current_user].present?
+          User.find_by!(domain: session[:current_user])
+        elsif cookies[:login_user].present? && cookies[:login_domain] == current_site.domain
+          user = User.find_by!(domain: cookies[:login_user])
+          session[:current_user] = user.domain
+          user
+        end
+      rescue ActiveRecord::RecordNotFound
+        logout_user
       end
-    rescue ActiveRecord::RecordNotFound
-      logout_user
+    end
+
+    def logout_user
+      session[:current_user] = nil
+      cookies.delete(:login_user, domain: current_site.domain)
+      cookies.delete(:login_domain, domain: current_site.domain)
     end
   end
 
-  def current_site
-    @current_site ||= load_current_site
-  end
-
-  def load_current_site
-    parts = request.subdomains + [request.domain]
-    while parts.any? && (site = User.hosted.find_by(domain: parts.join('.'))).nil?
-      parts.shift
+  concerning :CurrentSite do
+    included do
+      helper_method :current_site
     end
 
-    site
-  end
+    def current_site
+      @current_site ||= load_current_site
+    end
 
-  def logout_user
-    session[:current_user] = nil
-    cookies.delete(:login_user, domain: current_site.domain)
-    cookies.delete(:login_domain, domain: current_site.domain)
+    def load_current_site
+      parts = request.subdomains + [request.domain]
+      while parts.any? && (site = User.hosted.find_by(domain: parts.join('.'))).nil?
+        parts.shift
+      end
+
+      site
+    end
   end
 
   concerning :PageTitle do
