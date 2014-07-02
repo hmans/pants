34dd24c14a2455f8367a111ec8ac9b93e5d50308
Hendrik Mans <hendrik@mans.de>
2014-07-02 16:17:21 +0200

    Disable fist_of_fury on development, it was being weird.

--- config/initializers/fist_of_fury.rb
+++ config/initializers/fist_of_fury.rb
@@ -1,5 +1,5 @@
 # Ensure the jobs run only in a web server.
-if defined?(Rails::Server)
+if defined?(Rails::Server) && Rails.env.production?
   FistOfFury.attack! do
     UserPoller.recurs { minutely }
   end
