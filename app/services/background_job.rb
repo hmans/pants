84aca65e5c68b455c051380f573dfb7ee99d496c
Hendrik Mans <hendrik@mans.de>
2014-07-05 20:19:56 +0200

    Fix Celluloid integration

--- app/services/background_job.rb
+++ app/services/background_job.rb
@@ -1,5 +1,7 @@
 module BackgroundJob
-  include SuckerPunch::Job
+  def self.included(base)
+    base.send :include, SuckerPunch::Job
+  end
 
   def with_database(&blk)
     ActiveRecord::Base.connection_pool.with_connection(&blk)
