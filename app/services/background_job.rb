0ca9268a810446e61b3a65afe20a533da0740bc2
Hendrik Mans <hendrik@mans.de>
2014-07-05 20:26:23 +0200

    First stab at AppSignal integration.

--- app/services/background_job.rb
+++ app/services/background_job.rb
@@ -6,4 +6,28 @@ module BackgroundJob
   def with_database(&blk)
     ActiveRecord::Base.connection_pool.with_connection(&blk)
   end
+
+  def initialize(*args)
+    @time_created = Time.now
+    super
+  end
+
+  def with_appsignal(method = 'perform', &blk)
+    Appsignal::Transaction.create(SecureRandom.uuid, ENV.to_hash)
+
+    ActiveSupport::Notifications.instrument('perform_job.sucker_punch',
+      :class => self.class.to_s,
+      :method => method,
+      :queue_time => (Time.now - @time_created).to_f
+    ) do
+      yield
+    end
+  rescue Exception => exception
+    unless Appsignal.is_ignored_exception?(exception)
+      Appsignal::Transaction.current.add_exception(exception)
+    end
+    raise exception
+  ensure
+    Appsignal::Transaction.current.complete!
+  end
 end
