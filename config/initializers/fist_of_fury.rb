bf7cdbf5e9a81981a1c60c2e64890cdd697bd797
Hendrik Mans <hendrik@mans.de>
2014-07-10 22:02:41 +0200

    Another feeble attempt at making FistOfFury work within Passenger.

--- config/initializers/fist_of_fury.rb
+++ config/initializers/fist_of_fury.rb
@@ -3,3 +3,11 @@ if defined?(Rails::Server) && Rails.env.production?
   Rails.logger.info "Firing up FistOfFury"
   FistOfFury.attack!
 end
+
+# Make this thing work in Passenger, too.
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    Rails.logger.info "Firing up FistOfFury in new Passenger worker process"
+    FistOfFury.attack!
+  end
+end
