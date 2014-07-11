723fc6bdb8a76370b64097636997488d1dc58696
Hendrik Mans <hendrik@mans.de>
2014-07-12 01:34:47 +0200

    Simplify Pings listing

--- app/controllers/server/pings_controller.rb
+++ app/controllers/server/pings_controller.rb
@@ -2,7 +2,7 @@ class Server::PingsController < ServerController
   load_and_authorize_resource :ping
 
   def index
-    @pings = @pings.order('created_at DESC')
+    @pings = @pings.where('created_at > ?', 24.hours.ago).order('created_at DESC')
     respond_with @pings
   end
 end
